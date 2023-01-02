3c0d22ab9597d9e90bd1dbe67b8eff221700a349
Andrew P <40119314+andrewpayne68@users.noreply.github.com>
2023-01-02 22:18:47 +0000

    Fixed the Infura API Endpoint URL variable

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 import json
 infura_url = "https://mainnet.infura.io/v3/YOUR_INFURA_API_KEY_GOES_HERE"
 
-w3 = web3.Web3(web3.HTTPProvider(infura_url)) # your infura api endpoint
+w3 = web3.Web3(web3.HTTPProvider(infura_url)) # your infura api url endpoint
 cracked = json.load(open("cracked.json", "r")) # (eth)store public/private key of the account found
 zerobalance = json.load(open("zerobalance.json", "r")) # (no eth)store public/private key of the zero balance account
 
